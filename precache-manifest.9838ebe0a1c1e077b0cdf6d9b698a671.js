edeb38e7b56d717397611f3bc89891da6e5b1c26
barinovi <igorb202@gmail.com>
2020-10-07 21:16:56 -0400

    Updates

---
+++ precache-manifest.9838ebe0a1c1e077b0cdf6d9b698a671.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c1f474ad62c4c64cf5de7b48853e72",
+    "url": "/igor-barinov.github.io/index.html"
+  },
+  {
+    "revision": "0dbb056003388d9c315b",
+    "url": "/igor-barinov.github.io/static/css/main.fb40d10e.chunk.css"
+  },
+  {
+    "revision": "6af3e264699311795364",
+    "url": "/igor-barinov.github.io/static/js/2.f5caccc8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/igor-barinov.github.io/static/js/2.f5caccc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dbb056003388d9c315b",
+    "url": "/igor-barinov.github.io/static/js/main.a63fea64.chunk.js"
+  },
+  {
+    "revision": "34aa3508280a17543738",
+    "url": "/igor-barinov.github.io/static/js/runtime-main.883ec37c.js"
+  }
+]);
